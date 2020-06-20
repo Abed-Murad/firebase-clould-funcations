52995c0d4e242591538a4303b094876d5c346244
Abed Murad <Abdallah.Murad@ProtonMail.com>
2020-06-20 21:08:37 +0300

    Create AddMessage()

--- functions/index.js
+++ functions/index.js
@@ -10,3 +10,11 @@ const functions = require('firebase-functions');
 // The Firebase Admin SDK to access Cloud Firestore.
 const admin = require('firebase-admin');
 admin.initializeApp();
+
+
+exports.addMessage = funcations.https.onRequest(async (req, res) => {
+    const original = req.query.text;
+    const writeResult = await admin.firestore().collection('messages').add({original: original});
+    res.json({result: 'Message with ID: ${writeResult.id} added'});
+})
+
